3033e19baa797b2b7db444934c27373d39f4c269
anjaligh <anjali93ravindran@gmail.com>
2022-09-15 12:34:18 +0530

    bug fix

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -19,6 +19,17 @@ faPhoneVolume=faPhoneVolume;
 faEnvelope=faEnvelope;
 faLocationDot=faLocationDot;
 faPhone=faPhone;
+
+updateform={
+  firstname:'',
+  lastname:'',
+  phone:'',
+  mailid:'',
+  gender:'Male',
+  exper:'2',
+  address:''
+
+}
   constructor(private apicall:ApicallsService) { }
 
   ngOnInit(): void {
@@ -26,6 +37,11 @@ faPhone=faPhone;
       this.Data=JSON.parse(JSON.stringify(data))
       this.candidateData=JSON.parse(JSON.stringify(this.Data))
       console.log(this.candidateData)
+      this.updateform.firstname=this.candidateData[0].first_name;
+      this.updateform.lastname=this.candidateData[0].last_name;
+      this.updateform.mailid=this.candidateData[0].email;
+      this.updateform.phone=this.candidateData[0].phone_number;
+      this.updateform.address=this.candidateData[0].location;
       this.experience=JSON.parse(JSON.stringify(this.candidateData[0])).experience;
       console.log("experience");
       console.log(this.experience);
@@ -33,5 +49,13 @@ faPhone=faPhone;
       console.log(this.appliedJobs.length)
     })
   }
-
+  updateProfile(){
+    localStorage.setItem('firstname',this.updateform.firstname);
+    localStorage.setItem('lastname',this.updateform.lastname);
+    localStorage.setItem('phone',this.updateform.phone);
+    localStorage.setItem('mailid',this.updateform.mailid);
+    localStorage.setItem('gender',this.updateform.gender);
+    localStorage.setItem('address',this.updateform.address);
+    alert('Profile edited successfully');
+  }
 }
--- src/app/side-profile/side-profile.component.ts
+++ src/app/side-profile/side-profile.component.ts
@@ -74,3 +74,4 @@ export class SideProfileComponent implements OnInit {
     alert('Profile edited successfully');
   }
 }
+ 
